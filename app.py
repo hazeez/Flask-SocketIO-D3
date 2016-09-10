9517d45f3eb3d816ff80cbfe1c1aa97d7b659133
hazeez <hafizul.azeez@gmail.com>
2016-09-11 03:09:56 +0530

    working chat application without d3

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+app.config['SECRET_KEY'] = '!secret'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def broadcast_message(message):
+    emit('my_response', {'data': message['data']}, broadcast=True)
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my_response', {'data': 'Connected'})
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
