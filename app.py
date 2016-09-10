d9d1865af7c96f00d6609789f390dee74f815048
hazeez <hafizul.azeez@gmail.com>
2016-09-11 04:17:04 +0530

    added meaningful comments where missed

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
-
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.config['SECRET_KEY'] = '!secret'
@@ -12,11 +11,14 @@ def index():
     return render_template('index.html')
 
 
+# get the custom message event from the client and execute the respective
+# function in the server
 @socketio.on('my_broadcast_event', namespace='/test')
 def broadcast_message(message):
     emit('my_response', {'data': message['data']}, broadcast=True)
 
 
+# execute this when the client establishes a connection
 @socketio.on('connect', namespace='/test')
 def test_connect():
     emit('my_response', {'data': 'Connected'})
